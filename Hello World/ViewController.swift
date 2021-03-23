fb751dc407ce83af565380c6ed5ef59c05ef3352
Ananya George <ananyageorge019@gmail.com>
2021-03-23 14:08:24 +0530

    First iteration of Hello World
    Added button, text field and label
    Changed background color
    Rounded button corners

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Output: UILabel!
+    @IBOutlet weak var Input: UITextField!
+    @IBAction func Run(_ sender: UIButton) {
+        // the code in here is executed when the button is pressed
+        Output.text = "Hey, " + (Input.text)! + "!"
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
